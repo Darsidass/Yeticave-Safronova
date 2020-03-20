8230af1632d52f56166ff4b084d7e2e22e002da5
Darsidass <62241004+Darsidass@users.noreply.github.com>
2020-03-20 10:07:29 +0300

    fourth task

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require_once ('functions.php');
+
+$page_content = include_template('index.php', [
+    'categories' => $categories,
+    'ad' => $ad
+]);
+
+$layout_content = include_template('layout.php', [
+    'page_title' => 'Главная страница',
+    'is_auth' => $is_auth,
+    'user_avatar' => $user_avatar,
+    'user_name' => $user_name,
+    'page_content' => $page_content,
+    'categories' => $categories
+]);
+
+print($layout_content);
+?>
